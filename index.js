da40afaf773a750ceef392149c04a319462a7c3d
Ankur Kumar <52786629+ankur29mac@users.noreply.github.com>
2021-08-20 13:04:40 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,75 @@
+var readlineSync = require("readline-sync");
+
+var score = 0;
+
+// data of high score
+var highScores = [
+  {
+    name: "Kaustubh",
+    score: 3,
+  },
+
+  {
+    name: "Mritunjay",
+    score: 2,
+  },
+]
+
+// array of objects
+var questions = [{
+  question: "Where do I live? ",
+  answer: "Bengaluru"
+}, {
+  question: "My favorite programming language is? ",
+  answer: "JavaScript"
+},
+{
+  question: "In Bengaluru where do i live? ",
+  answer: "Marathahalli"
+}];
+
+function welcome() {
+ var userName = readlineSync.question("What's your name? ");
+
+  console.log("Welcome "+ userName + " to DO YOU KNOW ANKUR?");
+}
+
+
+// play function
+function play(question, answer) {
+  var userAnswer = readlineSync.question(question);
+
+  if (userAnswer.toUpperCase() === answer.toUpperCase()) { // branching
+    console.log("right!");
+    score = score + 1;
+    
+  } else {
+    console.log("wrong!");
+   
+  }
+
+  console.log("current score: ", score);
+  console.log("----------------------")
+}
+
+function game() {
+  for (var i=0; i<questions.length; i++) {
+    var currentQuestion = questions[i];
+    play(currentQuestion.question, currentQuestion.answer)
+  }
+}
+
+function showScores() {
+  console.log(" kudos! You SCORED: ", score);
+
+  console.log("Check out the high scores, if you should be there ping me and I'll update it in scoreboard");
+
+  highScores.map(score => console.log(score.name, " : ", score.score))
+}
+
+
+welcome();
+game();
+showScores();
+
+
